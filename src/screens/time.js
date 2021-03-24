2aa1571041f2576f91d252d44136e79132bfc124
Paul_Killian <57373172+PaulKillian@users.noreply.github.com>
2021-03-23 17:35:10 -0700

    Update time.js

--- src/screens/time.js
+++ src/screens/time.js
@@ -1,46 +1,99 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { db } from '../config';
-import { Button, ListItem } from 'react-native-elements';
-import { buttonContainer } from '../styles';
-import styled from 'styled-components/native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {Button} from 'react-native-elements';
+import {View, Text, StyleSheet} from 'react-native';
 
 const CaptureTime = () => {
-	const [time, setTime] = useState('')
-	const [id, setId] = useState(0)
+  const [timeIn, setTimeIn] = useState('');
+  const [timeOut, setTimeOut] = useState('');
+  const [clockinButtonStatus, setClockinButtonStatus] = useState(false);
+  const [clockoutButtonStatus, setClockoutButtonStatus] = useState(true);
 
-	const capture = () => {
-		const date = new Date().getDate(); //Current Date
-		const month = new Date().getMonth() + 1; //Current Month
-		const year = new Date().getFullYear(); //Current Year
-		const hours = new Date().getHours(); //Current Hours
-		const min = new Date().getMinutes(); //Current Minutes
-		const sec = new Date().getSeconds(); //Current Seconds
-		setTime(
-			`${date + '/' + month + '/' + year
-				+ ' ' + hours + ':' + min + ':' + sec}`
-		);
-	}
+  const capture = id => {
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth() + 1;
+    const date = new Date().getDate();
+    const hours = new Date().getHours();
+    const min = new Date().getMinutes();
+    const sec = new Date().getSeconds();
 
-	const ClockIn = styled.View`
-		display: flex;
-		justify-content: center;
-	`;
+    id === 'in'
+      ? (setTimeIn(
+          `${
+            month +
+            '/' +
+            date +
+            '/' +
+            year +
+            ' ' +
+            hours +
+            ':' +
+            min +
+            ':' +
+            sec
+          }`,
+        ),
+        setClockoutButtonStatus(false),
+        setClockinButtonStatus(true))
+      : (setTimeOut(
+          `${
+            month +
+            '/' +
+            date +
+            '/' +
+            year +
+            ' ' +
+            hours +
+            ':' +
+            min +
+            ':' +
+            sec
+          }`,
+        ),
+        setClockoutButtonStatus(true),
+        setClockinButtonStatus(false));
+  };
 
-	const ClockInButton = styled.Button`
-		width: 50%;
-	`;
+  const clockIn = id => {
+    capture(id);
+  };
 
-	const ClockInText = styled.Text`
-		text-align: center
-	`;
+  const clockOut = id => {
+    capture(id);
+  };
 
-	return (
-		<ClockIn>
-			<ClockInButton title="Clock In" onPress={capture}></ClockInButton>
-			<ClockInText>{time}</ClockInText>
-		</ClockIn>
-	)
-}
+  return (
+    <>
+      <View style={styles.content}>
+        <Button
+          disabled={clockinButtonStatus}
+          key={'in'}
+          title="Clock In"
+          onPress={id => clockIn('in')}
+        />
+        <Text>{timeIn}</Text>
+        <Button
+          buttonStyle={{
+            backgroundColor: 'green',
+          }}
+          disabled={clockoutButtonStatus}
+          key={'out'}
+          title="Clock Out"
+          onPress={id => clockOut('out')}
+        />
+        <Text>{timeOut}</Text>
+      </View>
+    </>
+  );
+};
+
+var styles = StyleSheet.create({
+  content: {
+    flex: 2,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default CaptureTime;
